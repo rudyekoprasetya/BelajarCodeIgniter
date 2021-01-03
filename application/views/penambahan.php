969103458d4cccdc5510dfcba50cb277cae4157f
Rudy Eko Prasetya <rudyekoprasetya@yahoo.co.id>
2021-01-03 10:27:19 +0700

    [DEL] Repo Lama

--- application/views/penambahan.php
+++
@@ -1,9 +0,0 @@
-<?php echo form_open('hitung/penambahan');?>
-<?php echo form_input('angka1',$angka1); ?> +
-<?php echo form_input('angka2',$angka2); ?>
-<br>
-<?php echo form_submit('submit','HITUNG'); ?>
-<?php echo form_close();?>
-<br>
-hasil : <?php echo $hasil;?>
-<li><?php echo anchor('hitung','Kembali');?></li>
